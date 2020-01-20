031fdf7ebc05c89f4708b1ce4e547f9989df490a
Aaron <aaronhynes93@gmail.com>
2020-01-20 13:56:07 +0000

    More Kotlin practice. Finding the Nth number of PI or the first occurrence of a chosen number.

---
+++ src/FindPI.kt
@@ -0,0 +1,54 @@
+const val PI = 3.14159265359
+val PI_ARRAY = arrayOf(1,4,1,5,9,2,6,5,3,5,9)
+
+fun main() {
+
+    println("What function do you wish to do?")
+    println("1. Find the Nth number of PI.")
+    println("2. Find the first occurrence of a number.")
+    var funcToDo = readLine()
+    if (funcToDo != null) {
+        determineFunc(funcToDo.toInt())
+    }
+
+
+}
+
+fun determineFunc(func: Int) {
+
+    when(func){
+        1 -> findNthInPI(findDigit())
+        2 -> findNumberInPI(findDigit())
+    }
+
+}
+
+fun findNumberInPI(findDigit: Int?) {
+
+    var number = PI_ARRAY.indexOf(findDigit)
+    number += 1
+    println("The first occurrence of $findDigit in PI is in position $number")
+}
+
+fun findDigit(): Int? {
+
+        println("Please enter a number.")
+        var digit = readLine()
+        return digit?.toInt()
+}
+
+fun findNthInPI(digitToFind: Int?) {
+
+    try {
+        if (digitToFind != null) {
+            println("The $digitToFind th of PI is: " + PI_ARRAY[digitToFind - 1])
+        }else{
+            println("Enter a valid number please!")
+            findDigit()
+        }
+    }catch(e: ArrayIndexOutOfBoundsException){
+        println("Please pick a smaller number!")
+        findDigit()
+    }
+
+}
