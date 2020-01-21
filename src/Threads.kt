a9856c07ed53a06e8ec45c20b44cb534ad6667ce
Aaron <aaronhynes93@gmail.com>
2020-01-21 17:11:06 +0000

    More Kotlin practice. Threads

---
+++ src/Threads.kt
@@ -0,0 +1,24 @@
+import kotlin.concurrent.thread
+
+class SimpleThreads: Thread(){
+
+    override fun run() {
+        println("${Thread.currentThread()} from SimpleThreads has run!")
+    }
+
+}
+
+class SimpleRunnable: Runnable{
+    override fun run() {
+        println("${Thread.currentThread()} from SimpleRunnable has run!")    }
+}
+
+fun main(){
+    val thread = SimpleThreads()
+    thread.start()
+
+    val runnable = SimpleRunnable()
+
+    val thread1 = Thread(runnable)
+    thread1.start()
+}
